007a3271928785bc0f4351543b5d44efe5c644c0
Nisum <Nisum@DESKTOP-IH9416U>
2017-12-17 01:06:48 +0530

    Fourth time commit using only "Commit".

--- SpringBootFirstApp/src/main/java/com/nisum/Rough.java
+++ SpringBootFirstApp/src/main/java/com/nisum/Rough.java
@@ -8,9 +8,13 @@ import java.util.TreeMap;
 
 public class Rough {
   public static void main(String[] args) {
-	for(int j=1; j<= 5; j++) {
+	  int n=5;
+	for(int j=1; j<= n; j++) {
 		for(int i=1; i<= j; i++) {
-			System.out.print("*");
+			System.out.print("?");
+		}
+		for(int i=1; i<= n-j+1;i++) {
+			System.out.print("8");
 		}
 		System.out.println();
 	}

1ec2493c8cfded7da670b890f1af3ee2a7431ff0
Nisum <Nisum@DESKTOP-IH9416U>
2017-12-16 09:00:56 +0530

    Second commit

--- SpringBootFirstApp/src/main/java/com/nisum/Team.java
+++ SpringBootFirstApp/src/main/java/com/nisum/Team.java
@@ -1,10 +1,18 @@
 package com.nisum;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Team {
  private int teamSize;
  private String teamLead;
  private String techLead;
+ private String prgmManager;
+ private String countryHead;
 }
